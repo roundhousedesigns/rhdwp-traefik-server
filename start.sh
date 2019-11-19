a4ad14678ac17944724e39d0939a67ea15ed7a45
Nick Gaswirth <nick@roundhouse-designs.com>
2019-11-19 04:10:14 +0000

    Start script runs traefik

--- start.sh
+++ start.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 # Create acme.json for LetsEncrypt storage
-sudo touch traefik/acme.json
-sudo chown www-data:www-data traefik/acme.json
-sudo chmod 600 traefik/acme.json
+sudo touch ./traefik/acme.json
+sudo chown www-data:www-data ./traefik/acme.json
+sudo chmod 600 ./traefik/acme.json
+
+# Start traefik
+docker-compose -f ./traefik/docker-compose.yml up -d
 
 # Die, and kill yourself
 rm "$0"
