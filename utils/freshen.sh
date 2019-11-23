9af71e3ac98f55be7f23006c191dcb6ac714b1fc
Nick Gaswirth <nick@roundhouse-designs.com>
2019-11-23 15:58:56 -0500

    cleanup

--- utils/freshen.sh
+++ utils/freshen.sh
@@ -16,9 +16,8 @@ for d in /srv/rhdwp/www/*; do
 	
 	# Rebuild
 	cd "${d}" && bash build.sh -r && cd "${wd}"
-
+	
 	# shuffle salts (bug in docker wordpress)
 	# docker-compose run --rm wp-cli config shuffle-salts
-
 done
 docker system prune --volumes -f
