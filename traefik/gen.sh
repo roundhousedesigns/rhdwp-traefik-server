1213a340fb7480197de4749dc03c8f1a9376352d
Nick Gaswirth <nick@roundhouse-designs.com>
2019-12-02 00:19:13 -0500

    cleanups

--- traefik/gen.sh
+++ traefik/gen.sh
@@ -5,11 +5,11 @@ set -e
 ## Generates traefik.toml with correct hostname
 ####
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-env_file="${parent_path}"/.env
-config_file="${parent_path}"/traefik.toml
+env_file="${parent_path}/.env"
+config_file="${parent_path}/traefik.toml"
 
 # Generate hostname var (overwriting)
 echo "HOSTNAME=$(hostname -f)" > "${env_file}"
 
-cp "${parent_path}"/traefik-template.toml "${config_file}"
+cp "${parent_path}/traefik-template.toml" "${config_file}"
 sed -i "s/%%hostname%%/"$(hostname -f)"/" "${config_file}"
