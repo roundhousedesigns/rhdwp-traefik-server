6d8f2fc1351c857bc363b3edec0c17c200a8a8c2
Nick Gaswirth <nick@roundhouse-designs.com>
2019-12-02 00:08:43 -0500

    rename

---
+++ utils/update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+for d in /srv/rhdwp/www/*; do
+	if [ -d "${d}" ]; then
+		docker_compose="${d}"/docker-compose.yml
+
+		docker-compose -f "${docker_compose}" --log-level ERROR run --rm wp-cli plugin update --all
+		docker-compose -f "${docker_compose}" --log-level ERROR run --rm wp-cli theme update --all
+		docker-compose -f "${docker_compose}" --log-level ERROR run --rm wp-cli core update
+	fi
+done
