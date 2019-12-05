b3267e144f968f4eddd40edd74696f5525d26e80
Nick Gaswirth <nick@roundhouse-designs.com>
2019-12-04 22:58:28 -0500

    script debug off

--- start.sh
+++ start.sh
@@ -6,22 +6,22 @@ secrets=./.secrets
 ## Verify secrets
 ask_secrets() {
 	# CloudFlare API Key, if not found in secrets
-	while [[ -z "${CF_API_KEY}" ]]; do
+	while [[ -z "$CF_API_KEY" ]]; do
 		read -r -p "CloudFlare API Key: " CF_API_KEY
 	done
 
 	# Mailgun API Key, if not found in secrets
-	while [[ -z "${MG_API_KEY}" ]]; do
+	while [[ -z "$MG_API_KEY" ]]; do
 		read -r -p "MailGun API Key: " MG_API_KEY
 	done
 
 	# SMTP login for dev mode, if not found in secrets
-	while [[ -z "${DEV_SMTP_LOGIN}" ]]; do
+	while [[ -z "$DEV_SMTP_LOGIN" ]]; do
 		read -r -p "RHDEV SMTP Login (Mailgun): " DEV_SMTP_LOGIN
 	done
 
 	# Mailgun API Key, if not found in secrets
-	while [[ -z "${DEV_SMTP_PASS}" ]]; do
+	while [[ -z "$DEV_SMTP_PASS" ]]; do
 		read -r -p "RHDEV SMTP Pass (Mailgun): " DEV_SMTP_PASS
 	done
 	
@@ -31,33 +31,41 @@ ask_secrets() {
 
 ## Write to .secrets and lock it down
 write_secrets() {
-	sudo tee -a "${secrets}" > /dev/null <<-EOT
-		CF_API_KEY="${CF_API_KEY}"
-		MG_API_KEY="${MG_API_KEY}"
-		DEV_SMTP_LOGIN="${DEV_SMTP_LOGIN}"
-		DEV_SMTP_PASS="${DEV_SMTP_PASS}"
-		WORDPRESS_SMTP_FROM="${WORDPRESS_SMTP_FROM}"
+	sudo tee -a "$secrets" > /dev/null <<-EOT
+		CF_API_KEY="$CF_API_KEY"
+		MG_API_KEY="$MG_API_KEY"
+		DEV_SMTP_LOGIN="$DEV_SMTP_LOGIN"
+		DEV_SMTP_PASS="$DEV_SMTP_PASS"
+		WORDPRESS_SMTP_FROM="$WORDPRESS_SMTP_FROM"
 	EOT
 	
-	sudo chmod 600 "${secrets}" && sudo chown www-data:www-data "${secrets}"
+	sudo chmod 600 "$secrets" && sudo chown www-data:www-data "$secrets"
 }
 
 # LetsEncrypt storage
 acme=./traefik/acme.json
-if [[ ! -f "${acme}" ]]; then
-	sudo touch "${acme}"
-	sudo chown www-data:www-data "${acme}"
-	sudo chmod 600 "${acme}"
+if [[ ! -f "$acme" ]]; then
+	sudo touch "$acme"
+	sudo chown www-data:www-data "$acme"
+	sudo chmod 600 "$acme"
 fi
 
 # wp-cli permissions
 sudo chown -R www-data:www-data ./wp-cli
 
 # Generate secrets
-if [[ -r "${secrets}" ]]; then
+if [[ -f "$secrets" ]]; then
+	# Unlock secrets
+	sudo chown "$USER" "$secrets"
+
 	# shellcheck disable=SC1091
 	# shellcheck source=/srv/rhdwp/.secrets
-	source "${secrets}"
+	. "$secrets"
+
+	# Re-lock secrets
+        sudo chown www-data:www-data "$secrets"
+        sudo chmod 600 "$secrets"
+
 else
 	ask_secrets
 	write_secrets
@@ -70,4 +78,4 @@ fi
 [[ ! -d ./www ]] && mkdir www
 
 # Start traefik
-( cd ./traefik && docker-compose up -d --remove-orphans )
+( cd ./traefik && docker-compose up -d --remove-orphans --force-recreate)
