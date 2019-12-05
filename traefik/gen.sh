d180b35ff036dcbf8d2c13aed9cad182224681f9
Nick Gaswirth <admin@roundhouse-designs.com>
2019-12-05 15:13:42 -0500

    working base

--- traefik/gen.sh
+++ traefik/gen.sh
@@ -8,7 +8,7 @@ HOSTNAME=$(hostname)
 export HOSTNAME
 
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-config_file="${parent_path}/traefik.toml"
+config_file="${parent_path}/docker-compose.yml"
 
-cp "${parent_path}/traefik-template.toml" "$config_file"
+cp "${parent_path}/docker-compose-template.yml" "$config_file"
 sed -i "s/%%domain%%/$(hostname -f)/" "$config_file"
