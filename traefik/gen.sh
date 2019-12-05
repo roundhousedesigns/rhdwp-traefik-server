00bf683cc09fce9aeb6fc55405cb2b720da36b5e
Nick Gaswirth <nick@roundhouse-designs.com>
2019-12-04 19:08:44 -0500

    simplified hostname replacement; disabled traefik from cert gen

--- traefik/gen.sh
+++ traefik/gen.sh
@@ -4,14 +4,11 @@ set -e
 ####
 ## Generates traefik.toml with correct hostname
 ####
+HOSTNAME=$(hostname)
+export HOSTNAME
+
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-env_file="${parent_path}/.env"
 config_file="${parent_path}/traefik.toml"
-fqdn=$(hostname -f)
 
-# Generate hostname var (overwriting)
-echo "FQDN=${fqdn}" > "${env_file}"
-echo "HOSTNAME=$(hostname)" >> "${env_file}"
-
-cp "${parent_path}/traefik-template.toml" "${config_file}"
-sed -i "s/%%fqdn%%/${fqdn}/" "${config_file}"
+cp "${parent_path}/traefik-template.toml" "$config_file"
+sed -i "s/%%domain%%/$(hostname -f)/" "$config_file"
