0de2670a44f2899086801b493c56e68a8746f5d1
Nick Gaswirth <admin@roundhouse-designs.com>
2020-01-09 10:00:41 -0500

    Added traefik stack update

--- utils/update.sh
+++ utils/update.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 set -e
 
-for d in /srv/rhdwp/www/*; do
-  if [ -d "${d}" ]; then
+utilsDir="$( cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd )"
+rootDir="$(dirname "${utilsDir}")"
+wwwDir="${rootDir}/www"
+
+## Update Traefik stack
+echo "UPDATE Traefik"
+git -C "$rootDir" checkout master
+git -C "$rootDir" pull -q
+
+for d in "$wwwDir"/*; do
+  if [ -d "$d" ]; then
     docker_compose="${d}"/docker-compose.yml
 
-    docker-compose -f "${docker_compose}" --log-level ERROR run --rm wp-cli plugin update --all
-    docker-compose -f "${docker_compose}" --log-level ERROR run --rm wp-cli theme update --all
-    docker-compose -f "${docker_compose}" --log-level ERROR run --rm wp-cli core update
+    docker-compose -f "$docker_compose" --log-level ERROR run --rm wp-cli plugin update --all
+    docker-compose -f "$docker_compose" --log-level ERROR run --rm wp-cli theme update --all
+    docker-compose -f "$docker_compose" --log-level ERROR run --rm wp-cli core update
   fi
 done
